8b25b35cc7688d8f23aa62ba054cfeacb510ef9f
slavon213 <slavon213@gmail.com>
2024-10-18 15:33:58 +0300

    add model and methods

---
+++ HomeWorkAnswers/task03/js/model.js
@@ -0,0 +1,93 @@
+const library = {
+    books: [
+        {
+            id: 1,
+            title: "Грокаємо алгоритми. Ілюстрований посібник для програмістів і допитливих",
+            author: " Адітья Бхаргавал",
+            year: 2024,
+            genre: "Навчальна література",
+        },
+
+        {
+            id: 2,
+            title: "1984",
+            author: "Джордж Орвелл",
+            year: 1949,
+            genre: "Антиутопія",
+        },
+        {
+            id: 3,
+            title: "Навчитися вчитися. Як запустити свій мозок на повну",
+            author: "Барбара Оклі",
+            year: 2019,
+            genre: "Навчальна література",
+        },
+        {
+            id: 4,
+            title: "Гаррі Поттер і філософський камінь",
+            author: "Дж. К. Роулінг",
+            year: 1997,
+            genre: "Фентезі",
+        },
+        {
+            id: 5,
+            title: "Гра престолів",
+            author: "Джордж Р. Р. Мартін",
+            year: 1996,
+            genre: "Фентезі",
+        },
+        {
+            id: 6,
+            title: "Старий і море",
+            author: "Ернест Хемінгуей",
+            year: 1952,
+            genre: "Пригодницький роман",
+        },
+        {
+            id: 7,
+            title: "Автоматизація рутинних завдань за допомогою Python. Практичний посібник для початківців",
+            author: "Ел Свейгарт",
+            year: 2017,
+            genre: "Навчальна література",
+        },
+    ],
+
+    lastId() {
+        let allId = this.books.map((book) => book.id);
+        return Math.max(...allId);
+    },
+
+    add(book) {
+        book.id = this.lastId() + 1;
+    },
+
+    findBook(id) {
+        let index = this.books.findIndex((book) => book.id === id);
+        return this.books[index];
+    },
+
+    findBookByTitle(titleBook) {
+        let index = this.books.findIndex((book) => book.title === titleBook);
+        return this.books[index];
+        
+    },
+
+    update(bookToUpdate) {
+        let index = this.books.findIndex(book => book.id === bookToUpdate.id);
+        this.books[index] = bookToUpdate;
+    },
+
+    remove(id) {
+        let index = this.books.findIndex((book) => book.id === id);
+        this.books.splice(index, 1);
+    }, 
+};
+
+
+if (require.main === module) {
+    console.log(library.lastId());
+    console.log(library.findBook(7));
+    library.remove(7);
+    console.log(library.lastId());
+    console.log(library.findBookByTitle("Старий і море"));
+}
